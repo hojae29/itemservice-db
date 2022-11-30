d3c0eb5c923c10a6ee3224be4c9cae43012d20e0
hojae <gkqthd6386@naver.com>
2022-11-30 23:19:32 +0900

    H2 데이터베이스 임베디드 모드로 실행

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,18 +2,22 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * scanBasePackages = hello.itemservice.web 패키지 하위 파일만 컴포넌트 스캔
  * 컨트롤러에서 Service, Repository 에 의존하기 때문에 자동주입이 되지않는다.
  * @Import 어노테이션 사용해서 직접 빈으로 등록하면 설정파일 내 빈들로 의존성 주입이 된다.
  */
-
+@Slf4j
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
@@ -30,6 +34,24 @@ public class ItemServiceApplication {
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	/**
+	 * test 프로필일때만 동작
+	 * jdbc:h2:mem:db를 url로 지정하면 임베디드 모드(메모리 모드)로 동작하는 h2 데이터베이스를 사용할 수 있다
+	 * DB_CLOSE_DELAY=-1 임베디드 모드에서는 데이터베이스 커넥션 연결이 모두 끊어지면 데이터베이스도 종료되는데,
+	 * 그것을 방지하는 설정이다.
+	 */
+	@Bean
+	@Profile("test")
+	public DataSource dataSource(){
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
 
 }
